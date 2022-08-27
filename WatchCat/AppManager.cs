bec650852fb3f93b6940b2a24e754ab096e34dfd
Sflashy <metinkuruscu@hotmail.com>
2022-08-27 18:38:57 +0300

    added AppManager

---
+++ WatchCat/AppManager.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchCat
+{
+    public static class AppManager
+    {
+        public static async Task<dynamic> HttpRequest(string url)
+        {
+            using (var httpClient = new System.Net.Http.HttpClient())
+            {
+                var response = await httpClient.GetAsync(url);
+                if (!response.IsSuccessStatusCode) return null;
+                return JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result);
+            }
+        }
+    }
+}
