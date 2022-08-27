15506d84fb4bb74e9e007119676569f5d4b652e8
Sflashy <metinkuruscu@hotmail.com>
2022-08-28 01:15:34 +0300

    added show the item in warframe market

--- WatchCat/Items.xaml.cs
+++ WatchCat/Items.xaml.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using WatchCat.Models;
 
 namespace WatchCat
 {
     public partial class Items : Page
     {
-        private List<Item> _items;
+        private List<Item> _itemList;
+        public static Items Instance;
 
         public Items()
         {
             InitializeComponent();
+            Instance = this;
         }
 
         private void ItemSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (_items == null) return;
+            if (_itemList == null) return;
             var foundItems = new List<Item>();
-            foreach (var item in _items)
+            foreach (var item in _itemList)
             {
                 if (!string.IsNullOrEmpty(item.Name) && item.Name.ToLower().Contains(ItemSearch.Text.ToLower()))
                 {
@@ -33,25 +39,29 @@ namespace WatchCat
             }
             else
             {
-                DataGrid.ItemsSource = _items;
+                DataGrid.ItemsSource = _itemList;
             }
         }
 
         private async void OnLoaded(object sender, System.Windows.RoutedEventArgs e)
         {
+            await LoadItems();
+            if (_itemList == null) return;
+
+            DataGrid.ItemsSource = _itemList;
+
             while (true)
             {
-                await UpdateItems();
                 await UpdatePrices();
                 await Task.Delay(new TimeSpan(0, 5, 0));
             }
         }
 
-        private async Task UpdateItems()
+        private async Task LoadItems()
         {
-            _items = new List<Item>();
             dynamic items = await AppManager.HttpRequest("https://api.warframestat.us/wfinfo/filtered_items");
-
+            if (items == null) return;
+            _itemList = new List<Item>();
             foreach (dynamic item in items.eqmt)
             {
                 foreach (dynamic part in item.Value.parts)
@@ -60,12 +70,13 @@ namespace WatchCat
                     if (ducats == null) continue;
                     string name = part.Name;
                     bool vaulted = (part.Value.vaulted == "true") ? true : false;
-
-                    _items.Add(new Item
+                    string wfmLink = name.Replace(' ', '_').ToLower(); 
+                    _itemList.Add(new Item
                     {
                         Name = name,
                         Vaulted = vaulted,
-                        Ducats = int.Parse(ducats)
+                        Ducats = int.Parse(ducats),
+                        WFMUrl = "https://warframe.market/items/" + wfmLink
                     });
                 }
             }
@@ -74,18 +85,41 @@ namespace WatchCat
         private async Task UpdatePrices()
         {
             dynamic prices = await AppManager.HttpRequest("https://api.warframestat.us/wfinfo/prices");
-
+            if (prices == null) return;
             foreach (dynamic item in prices)
             {
                 string itemName = item.name;
                 if (Regex.Match(itemName, @"Chassis|System|Neuroptics|Wings").Success) itemName = itemName.Replace(" Blueprint", "");
-                Item _item = _items.FirstOrDefault<Item>(x => x.Name == itemName);
+                Item _item = _itemList.FirstOrDefault(x => x.Name == itemName);
                 if (_item == null) continue;
                 _item.Avarage = item.custom_avg;
                 _item.Volume = item.today_vol;
             }
+        }
 
-            DataGrid.ItemsSource = _items;
+        private void OnRowDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            IInputElement element = e.MouseDevice.DirectlyOver;
+            if (element != null && element is FrameworkElement)
+            {
+                if (((FrameworkElement)element).Parent is DataGridCell)
+                {
+                    var grid = sender as DataGrid;
+                    if (grid != null && grid.SelectedItems != null && grid.SelectedItems.Count == 1)
+                    {
+                        var selectedItem = grid.SelectedItem as Item;
+                        if (selectedItem != null)
+                        {
+                            OpenInBrowser(selectedItem.WFMUrl);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OpenInBrowser(string url)
+        {
+            Process.Start(url);
         }
     }
 }
