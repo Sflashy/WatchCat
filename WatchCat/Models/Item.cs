e57c0b637cc99a71ab6503b59d73546ed6d1c1b5
Sflashy <metinkuruscu@hotmail.com>
2022-08-28 01:12:43 +0300

    added WFMUrl and changed to property model

---
+++ WatchCat/Models/Item.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WatchCat.Models
+{
+    public class Item : INotifyPropertyChanged
+    {
+        private double _avarage;
+        private int _volume;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Name { get; set; }
+        public double Avarage 
+        { 
+            get { return _avarage; }
+            set 
+            { 
+                if(value != _avarage)
+                {
+                    _avarage = value;
+                    NotifyPropertyChanged();
+                }
+            } 
+        }
+        public int Ducats { get; set; }
+        public bool Vaulted { get; set; }
+        public int Volume 
+        { 
+            get { return _volume; } 
+            set 
+            { 
+                if(value != _volume)
+                {
+                    _volume = value;
+                    NotifyPropertyChanged();
+                }
+            } 
+        }
+        public string WFMUrl;
+    }
+}
