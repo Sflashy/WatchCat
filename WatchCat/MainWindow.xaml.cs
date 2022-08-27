1cce4309cd59231497de6830bc777a3591d79886
Sflashy <metinkuruscu@hotmail.com>
2022-08-27 18:40:32 +0300

    added MainWindow

--- WatchCat/MainWindow.xaml.cs
+++ WatchCat/MainWindow.xaml.cs
@@ -15,14 +15,35 @@ using System.Windows.Shapes;
 
 namespace WatchCat
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Source = new Uri("Items.xaml", UriKind.RelativeOrAbsolute);
+        }
+
+        private void MinimizeApp(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void OnWindowStateChanged(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized;
+        }
+
+        private void CloseApp(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
         }
     }
 }
