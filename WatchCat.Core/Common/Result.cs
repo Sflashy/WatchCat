8afe2c5217d036d30b6d2c445c288d08ba1e90aa
Sflashy <github@sflashy.com>
2025-02-24 15:59:17 +0300

    Add Result class and update project structure
    Introduces a new `Result<T>` class and `IResult<T>` interface for operation outcomes. Adds `ToObservableCollectionExtension` for converting `IEnumerable<T>` to `ObservableCollection<T>`. Enhances `Data` class to process dynamic equipment data into `Part` objects. Updates `HttpClientService` to utilize the new `Result<T>` for HTTP responses. Modifies project file to include necessary package references and updates solution file for new project structure.

---
+++ WatchCat.Core/Common/Result.cs
@@ -0,0 +1,78 @@
+﻿namespace WatchCat.Core.Common;
+
+/// <summary>
+/// Represents the result of an operation, which may produce data of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The type of data that the result may contain.</typeparam>
+public class Result<T> : IResult<T>
+{
+    /// <summary>
+    /// Gets a value indicating whether the operation was successful.
+    /// </summary>
+    public bool IsSuccess { get; }
+
+    /// <summary>
+    /// Gets the data produced by the operation. This can be <see langword="null"/> if the operation was not successful.
+    /// </summary>
+    public T Data { get; }
+
+    /// <summary>
+    /// Gets a message that provides additional information about the result. This can be <see langword="null"/>.
+    /// </summary>
+    public string Message { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Result{T}"/> class.
+    /// </summary>
+    /// <param name="isSuccess">A value indicating whether the operation was successful.</param>
+    /// <param name="data">The data produced by the operation. This can be <see langword="null"/>.</param>
+    /// <param name="message">A message that provides additional information about the result. This can be <see langword="null"/>.</param>
+    public Result(bool isSuccess, T data = default, string message = null)
+    {
+        IsSuccess = isSuccess;
+        Data = data;
+        Message = message;
+    }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="Result{T}"/> class representing a successful operation with the specified data.
+    /// </summary>
+    /// <param name="data">The data produced by the operation.</param>
+    /// <returns>A <see cref="Result{T}"/> instance representing a successful operation.</returns>
+    public static Result<T> Success(T data = default)
+    {
+        return new Result<T>(true, data);
+    }
+
+    /// <summary>
+    /// Creates a new instance of the <see cref="Result{T}"/> class representing a failed operation with the specified message.
+    /// </summary>
+    /// <param name="message">A message that provides additional information about the result.</param>
+    /// <returns>A <see cref="Result{T}"/> instance representing a failed operation.</returns>
+    public static Result<T> Failure(string message = null)
+    {
+        return new Result<T>(false, default, message);
+    }
+}
+
+/// <summary>
+/// Represents the result of an operation that can produce data of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The type of the data that the result may contain.</typeparam>
+public interface IResult<T>
+{
+    /// <summary>
+    /// Gets a value indicating whether the operation was successful.
+    /// </summary>
+    bool IsSuccess { get; }
+
+    /// <summary>
+    /// Gets the data produced by the operation. This can be <see langword="null"/> if the operation was not successful.
+    /// </summary>
+    T Data { get; }
+
+    /// <summary>
+    /// Gets a message that provides additional information about the result. This can be <see langword="null"/>.
+    /// </summary>
+    string Message { get; }
+}
---
+++ WatchCat.Core/Models/Data.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Serilog;
+
+namespace WatchCat.Core.Models;
+
+public class Data
+{
+
+    [JsonProperty("eqmt")]
+    public dynamic Equipment { set
+        {
+            ProcessEquipment(value);
+        }
+    }
+
+    private void ProcessEquipment(dynamic value)
+    {
+        foreach (dynamic part in value)
+        {
+            foreach (dynamic data in part.Value.parts)
+            {
+                if (data.Value.ducats == null) continue;
+
+                Parts.Add(ConvertToPart(data));
+
+            }
+        }
+    }
+
+    private Part ConvertToPart(dynamic data)
+    {
+        Part part = new Part();
+        part.Name = data.Name;
+        part.Ducats = data.Value.ducats;
+        part.Vaulted = data.Value.vaulted;
+        foreach (var relicType in RawRelicData)
+        {
+            foreach (var relic in relicType.Value)
+            {
+                foreach (var kvp in relic.Value)
+                {
+                    if(part.Name == kvp.Value)
+                    {
+                        part.Relic = $"{relicType.Key} {relic.Key}";
+                    }
+                }
+            }
+        }
+        return part;
+    }
+
+    [JsonProperty("relics")]
+    public Dictionary<string, Dictionary<string, Dictionary<string, string>>> RawRelicData { get; set; }
+
+    public List<Part> Parts { get; set; } = new List<Part>();
+}
+
+public class Part
+{
+    public string Name { get; set; }
+    public int Ducats { get; set; }
+    public bool Vaulted { get; set; }
+    public double Average { get; set; }
+    public int Volume { get; set; }
+    public string Relic { get; set; }
+}
