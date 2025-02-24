eef5fe8044b5efa644a97575fd3502fefa4551aa
Sflashy <github@sflashy.com>
2025-02-24 15:58:36 +0300

    Add SearchBar and TopBar user controls
    This commit introduces two new user controls: `SearchBar` and `TopBar`.
    The `SearchBar` features a search input field with Material Design elements,
    including a `Card` and a `PackIcon`, and is bound to a `Query` property
    with command triggering on text changes. The `TopBar` includes a logo,
    window control buttons, and a progress indicator, all styled with
    Material Design. These additions enhance the application's UI for
    searching and window management.

---
+++ WatchCat/Controls/SearchBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WatchCat.Controls;
+
+public partial class SearchBar : UserControl
+{
+    public SearchBar()
+    {
+        InitializeComponent();
+    }
+}
---
+++ WatchCat/Controls/TopBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace WatchCat.Controls;
+
+public partial class TopBar : UserControl
+{
+    public TopBar()
+    {
+        InitializeComponent();
+    }
+}
